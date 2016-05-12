dd9cf08b3dd26a289f4bf96c0d8ad6a281f9cf02
Wieger Opmeer <opmeer@strato-rz.de>
2016-05-12 19:40:58 +0200

    change the error class argument into a text value

--- db/procs/do_raise_error.sql
+++ db/procs/do_raise_error.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION jobcenter.do_raise_error(a_jobtask jobtask, a_errmsg text, a_fatal boolean DEFAULT false)
+CREATE OR REPLACE FUNCTION jobcenter.do_raise_error(a_jobtask jobtask, a_errmsg text, a_class text DEFAULT 'normal'::text)
  RETURNS nextjobtask
  LANGUAGE plpgsql
  SET search_path TO jobcenter, pg_catalog, pg_temp
@@ -8,14 +8,13 @@ BEGIN
 	v_error = jsonb_build_object(
 		'error', jsonb_build_object(
 			'msg', a_errmsg,
-			'class', CASE WHEN a_fatal THEN 'fatal' ELSE 'normal' END
+			'class', a_class
 		)
 	);
 
 	UPDATE jobs SET
 		state = 'error',
 		task_completed = now(),
-		waitfortask_id = NULL,
 		timeout = NULL,
 		out_args = v_error
 	WHERE job_id = a_jobtask.job_id;
