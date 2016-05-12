da0893fdab4988c75e2a07916abbca58aec07d4a
Wieger Opmeer <opmeer@strato-rz.de>
2016-05-12 19:05:44 +0200

    finished jobs are move to the jobs_archive table by this funcion, callled from the maestro

---
+++ db/procs/do_archival_and_cleanup.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE FUNCTION jobcenter.do_archival_and_cleanup(dummy text DEFAULT 'dummy'::text)
+ RETURNS void
+ LANGUAGE sql
+ SECURITY DEFINER
+ SET search_path TO jobcenter, pg_catalog, pg_temp
+AS $function$
+-- mark dead/gone/whatever workers as stopped
+UPDATE workers SET
+	stopped = now()
+WHERE
+	stopped IS NULL
+	AND last_ping + interval '12 minutes' < now();
+-- move finished jobs to the jobs_archive table
+WITH jobrecords AS (
+	DELETE FROM
+		jobs
+	WHERE
+		state = 'finished'
+		AND job_finished < now() - interval '1 minute'
+	RETURNING
+		job_id,
+		workflow_id,
+		parentjob_id,
+		state,
+		arguments,
+		job_created,
+		job_finished,
+		stepcounter,
+		out_args,
+		environment,
+		max_steps
+)
+INSERT INTO jobs_archive SELECT * FROM jobrecords;
+$function$
