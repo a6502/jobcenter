3469bfb010906301680bb7ca09ad55dd1b484f5a
Wieger Opmeer <opmeer@strato-rz.de>
2016-05-12 18:58:05 +0200

    leftover that should have been in the last commit

--- db/procs/do_wfomap.sql
+++ db/procs/do_wfomap.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION jobcenter.do_wfmap(code text, vars jsonb)
+CREATE OR REPLACE FUNCTION jobcenter.do_wfomap(code text, vars jsonb)
  RETURNS jsonb
  LANGUAGE plperlu
  SET search_path TO jobcenter, pg_catalog, pg_temp
@@ -6,24 +6,25 @@ AS $function$
 
 use strict;
 use warnings;
+
 #use plperl.on_init instead
 #use lib '/home/wieger/src/jobcenter/lib';
-use JSON::MaybeXS;
+use JSON::MaybeXS qw(from_json to_json);
 use JobCenter::Safe;
 
 my $safe = new JobCenter::Safe;
 
 my ($code, $jvars) = @_;
 
-our %v = %{decode_json($jvars // '{}')};
+our %v = %{from_json($jvars // '{}')};
 our %o = ();
 
-$safe->share(qw(%v %o &decode_json &encode_json));
+$safe->share(qw(%v %o &from_json &to_json));
 
 $safe->reval($code, 1);
 
 die "$@" if $@;
 
-return encode_json(\%o);
+return to_json(\%o);
 
 $function$
