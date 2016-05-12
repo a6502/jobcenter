17c7f4d51444282bf914a5be8bb9e2b69a4dc12e
Wieger Opmeer <opmeer@strato-rz.de>
2016-05-12 19:13:30 +0200

    add a simple role based security model for clients and workers

--- db/procs/announce.sql
+++ db/procs/announce.sql
@@ -1,11 +1,16 @@
-CREATE OR REPLACE FUNCTION jobcenter.announce(a_workername text, a_actionname text)
+CREATE OR REPLACE FUNCTION jobcenter.announce(workername text, actionname text, impersonate text DEFAULT NULL::text)
  RETURNS TABLE(o_worker_id bigint, o_listenstring text)
  LANGUAGE plpgsql
  SECURITY DEFINER
  SET search_path TO jobcenter, pg_catalog, pg_temp
 AS $function$DECLARE
+	a_workername ALIAS FOR $1;
+	a_actionname ALIAS FOR $2;
+	a_impersonate ALIAS FOR $3;
 	v_worker_id bigint;
 	v_action_id int;
+	v_have_role text;
+	v_should_role text;
 	v_listenstring text;
 BEGIN
 	-- create worker if it does not exist already
@@ -19,15 +24,51 @@ BEGIN
 		INSERT INTO workers(name) VALUES (a_workername) INTO v_worker_id ON CONFLICT DO NOTHING RETURNING worker_id;
 		EXIT WHEN found; -- exit loop when insert succeeded
 	END LOOP;
-	SELECT action_id INTO v_action_id FROM actions WHERE name = a_actionname ORDER BY version DESC LIMIT 1;
+
+	SELECT
+		action_id, rolename
+		INTO v_action_id, v_should_role
+	FROM
+		actions
+	WHERE
+		name = a_actionname
+	ORDER BY version DESC LIMIT 1;
+
 	IF NOT found THEN
 		RAISE EXCEPTION 'no action named %.', a_actionname;
 	END IF;
+
+	-- check session user because we are in a security definer stored procedure
+	IF a_impersonate IS NOT NULL THEN
+		-- check if the postgresql session user is allowed to impersonate role a_impersonate
+		PERFORM
+			true
+		FROM
+			jc_impersonate_roles
+		WHERE
+			rolename = session_user
+			AND impersonates = a_impersonate;
+
+		IF NOT FOUND THEN
+			RAISE EXCEPTION 'worker % has no right to impersonate role %', session_user, a_impersonate;
+		END IF;
+
+		v_have_role := a_impersonate;
+	ELSE
+		v_have_role := session_user;
+	END IF;
+
+	IF NOT do_check_role_membership(v_have_role, v_should_role) THEN
+		RAISE EXCEPTION 'worker % with role % has no permission for role %', session_user, v_have_role, v_should_role;
+	END IF;
+
 	BEGIN
 		INSERT INTO worker_actions(worker_id, action_id) VALUES (v_worker_id, v_action_id);
 	EXCEPTION WHEN unique_violation THEN
+		-- fixme: or just ignore?
 		RAISE EXCEPTION 'worker % already can do action %', a_workername, a_actionname;
 	END;
+
 	v_listenstring := 'action:' || v_action_id || ':ready';
 	RETURN QUERY VALUES (v_worker_id, v_listenstring);
 END$function$
--- db/procs/create_job.sql
+++ db/procs/create_job.sql
@@ -1,9 +1,13 @@
-CREATE OR REPLACE FUNCTION jobcenter.create_job(a_wfname text, a_args jsonb, a_tag text DEFAULT NULL::text)
+CREATE OR REPLACE FUNCTION jobcenter.create_job(wfname text, args jsonb, tag text DEFAULT NULL::text, impersonate text DEFAULT NULL::text)
  RETURNS TABLE(o_job_id bigint, o_listenstring text)
  LANGUAGE plpgsql
  SECURITY DEFINER
  SET search_path TO jobcenter, pg_catalog, pg_temp
 AS $function$DECLARE
+	a_wfname ALIAS FOR $1;
+	a_args ALIAS FOR $2;
+	a_tag ALIAS FOR $3;
+	a_impersonate ALIAS FOR $4;
 	v_workflow_id int;
 	v_task_id int;
 	v_key text;
@@ -15,6 +19,8 @@ AS $function$DECLARE
 	v_val jsonb;
 	v_env jsonb;
 	v_tags text[] DEFAULT ARRAY['default'];
+	v_have_role text;
+	v_should_role text;
 BEGIN
 	IF a_tag IS NOT NULL AND a_tag <> 'default' THEN
 		v_tags = string_to_array(a_tag, ':') || v_tags;
@@ -22,18 +28,43 @@ BEGIN
 
 	-- find the worklow by name
 	SELECT
-		action_id, wfenv INTO v_workflow_id, v_env
+		action_id, wfenv, rolename INTO
+		v_workflow_id, v_env, v_should_role
 	FROM 
 		actions
-		LEFT JOIN action_version_tags USING (action_id)
+		LEFT JOIN action_version_tags AS avt USING (action_id)
 	WHERE
 		type = 'workflow'
 		AND name = a_wfname
-		AND (tag = ANY(v_tags) OR tag IS NULL)
-	ORDER BY array_position(v_tags, tag), version DESC LIMIT 1;
+		AND (avt.tag = ANY(v_tags) OR avt.tag IS NULL)
+	ORDER BY array_position(v_tags, avt.tag), version DESC LIMIT 1;
 
 	IF NOT FOUND THEN
 		RAISE EXCEPTION 'no workflow named %', a_wfname;
+	END IF;
+
+	-- check session user because we are in a security definer stored procedure
+	IF a_impersonate IS NOT NULL THEN
+		-- check if the postgresql session user is allowed to impersonate role a_impersonate
+		PERFORM
+			true
+		FROM
+			jc_impersonate_roles 
+		WHERE
+			rolename = session_user
+			AND impersonates = a_impersonate;
+
+		IF NOT FOUND THEN
+			RAISE EXCEPTION 'client % has no right to impersonate role %', session_user, a_impersonate;
+		END IF;
+
+		v_have_role := a_impersonate;
+	ELSE
+		v_have_role := session_user;
+	END IF;
+
+	IF NOT do_check_role_membership(v_have_role, v_should_role) THEN
+		RAISE EXCEPTION 'client % with role % has no permission for role %', session_user, v_have_role, v_should_role;
 	END IF;
 
 	-- check parameters
@@ -47,20 +78,20 @@ BEGIN
 			IF v_actual = 'object' THEN
 				SELECT fields INTO v_fields FROM jsonb_object_fields WHERE typename = v_type;
 				IF NOT v_val ?& v_fields THEN
-					RAISE EXCEPTION 'input parameter % with value % does have required fields %', v_key, v_val, v_fields;
+					RAISE EXCEPTION 'input parameter "%" with value "%" does have required fields %', v_key, v_val, v_fields;
 				END IF;
 			ELSIF v_actual = null OR v_actual = v_type THEN
 				-- ok?
 				NULL;
 			ELSE
-				RAISE EXCEPTION 'input parameter % has wrong type % (should be %)', v_key, v_actual, v_type;
+				RAISE EXCEPTION 'input parameter "%" has wrong type % (should be %)', v_key, v_actual, v_type;
 			END IF;
 		ELSE
 			IF v_opt THEN
 				-- ugh.. copy a_args?
 				a_args := jsonb_set(a_args, ARRAY[v_key], v_def);
 			ELSE
-				RAISE EXCEPTION 'required input parameter % not found', v_key;
+				RAISE EXCEPTION 'required input parameter "%" not found', v_key;
 			END IF;
 		END IF;
 
@@ -81,8 +112,9 @@ BEGIN
 		RAISE EXCEPTION 'no start task in workflow % .', a_wfname;
 	END IF;
 
-	SELECT jcenv FROM jcenv INTO v_val; -- hack, reuse v-val
-	v_env := v_env || v_val; -- jcenv overwrites wfenv
+	SELECT jcenv FROM jc_env INTO STRICT v_val; -- hack, reuse v-val
+	v_env := COALESCE(v_env, '{}'::jsonb) || v_val; -- jcenv overwrites wfenv
+	v_env := jsonb_set(v_env, '{client}', to_jsonb(session_user));
 
 	-- now create the new job and mark the start task 'done'
 	INSERT INTO jobcenter.jobs
