a1d581c7e4f30ddb6208d81677075d2387d3ff16
Wieger Opmeer <opmeer@strato-rz.de>
2016-05-12 19:14:45 +0200

    get_job_status has to look in the jobs_archive table too

--- db/procs/get_job_status.sql
+++ db/procs/get_job_status.sql
@@ -5,13 +5,19 @@ CREATE OR REPLACE FUNCTION jobcenter.get_job_status(a_job_id bigint)
  SET search_path TO jobcenter, pg_catalog, pg_temp
 AS $function$DECLARE
 	v_state job_state;
-	v_workflow_id integer;
 	v_out_args jsonb;
 BEGIN
+
 	SELECT 
-		workflow_id, state, out_args INTO v_workflow_id, v_state, v_out_args
-	FROM 
+		state, out_args INTO STRICT v_state, v_out_args
+	FROM
 		jobs
+	WHERE
+		job_id = a_job_id
+	UNION ALL SELECT
+		state, out_args
+	FROM
+		jobs_archive
 	WHERE
 		job_id = a_job_id;
 
