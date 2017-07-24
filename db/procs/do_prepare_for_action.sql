1c1afc2f2e7a7c8b4b718791ca7aeb13c04411bd
Wieger Opmeer <opmeer@strato-rz.de>
2017-07-24 14:01:35 +0200

    if this action allows filtering then only signal workers that match the filters

--- db/procs/do_prepare_for_action.sql
+++ db/procs/do_prepare_for_action.sql
@@ -4,15 +4,18 @@ CREATE OR REPLACE FUNCTION jobcenter.do_prepare_for_action(a_jobtask jobtask)
  SET search_path TO jobcenter, pg_catalog, pg_temp
 AS $function$DECLARE
 	v_action_id int;
+	v_conf jsonb;
 	v_args jsonb;
 	v_env jsonb;
 	v_vars jsonb;
 	v_in_args jsonb;
+	v_workers bigint[];
+	v_payload jsonb;
 BEGIN
-	-- get the arguments and variables
+	-- get the arguments and such
 	SELECT
-		action_id, arguments, environment, variables
-		INTO v_action_id, v_args, v_env, v_vars
+		action_id, config, arguments, environment, variables
+		INTO v_action_id, v_conf, v_args, v_env, v_vars
 	FROM 
 		actions
 		JOIN tasks USING (action_id)
@@ -40,7 +43,29 @@ BEGIN
 	WHERE
 		job_id = a_jobtask.job_id;
 
-	RAISE NOTICE 'NOTIFY "action:%:ready", %', v_action_id, a_jobtask.job_id;
-	PERFORM pg_notify('action:' || v_action_id || ':ready', a_jobtask.job_id::text);
-	RETURN null; -- no text task
+	-- if filtering is allowed
+	IF v_conf ? 'filter' THEN
+		-- see which workers have matching filters
+		SELECT
+			array_agg(worker_id) INTO v_workers
+		FROM
+			worker_actions
+		WHERE
+			action_id = v_action_id
+			AND (filter IS NULL
+			     OR v_in_args @> filter);
+
+		IF v_workers IS NULL THEN
+			RAISE NOTICE 'no worker for action_id % in_args %', v_action_id, v_in_args;
+			RETURN null; -- no next task
+		END IF;
+		-- RAISE NOTICE 'action_id % in_args % workers %',	v_action_id, v_in_args, v_workers;
+		v_payload = jsonb_build_object('job_id', a_jobtask.job_id, 'workers', v_workers);
+	ELSE
+		v_payload = jsonb_build_object('job_id', a_jobtask.job_id);
+	END IF;
+
+	RAISE NOTICE 'NOTIFY "action:%:ready", %', v_action_id, v_payload;
+	PERFORM pg_notify('action:' || v_action_id || ':ready', v_payload::text);
+	RETURN null; -- no next task
 END$function$
