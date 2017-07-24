02ad8968f754dd53284ab767f149f27c58ba5adc
Wieger Opmeer <opmeer@strato-rz.de>
2017-07-24 14:18:10 +0200

    add filtering support to retrywait casus: only wake up workers matching filter

--- db/procs/do_timeout.sql
+++ db/procs/do_timeout.sql
@@ -7,6 +7,10 @@ AS $function$DECLARE
 	v_job_id bigint;
 	v_task_id integer;
 	v_action_id integer;
+	v_conf jsonb;
+	v_payload jsonb;
+	v_in_args jsonb;
+	v_workers bigint[];
 	v_workflow_id integer;
 	v_state job_state;
 	v_next integer;
@@ -34,7 +38,12 @@ BEGIN
 			PERFORM do_task_done(v_jobtask, v_eventdata);
 		WHEN 'retrywait' THEN
 			-- simpels?
-			SELECT action_id INTO v_action_id FROM tasks WHERE task_id=v_task_id;
+			SELECT
+				action_id, config INTO v_action_id, v_conf
+			FROM
+				actions
+				JOIN tasks USING (action_id)
+			WHERE task_id=v_task_id;
 
 			UPDATE
 				jobs
@@ -43,11 +52,36 @@ BEGIN
 				timeout = null, -- done with this timeout
 				task_state = jsonb_set(COALESCE(task_state,'{}'::jsonb), '{tries}', to_jsonb(COALESCE((task_state->>'tries')::integer,0) + 1))
 			WHERE
-				job_id = v_job_id;
+				job_id = v_job_id
+			RETURNING
+				out_args INTO v_in_args;
 			-- RAISE NOTICE 'timeout for job %', v_job_id;
-			-- PERFORM do_task_done(v_jobtask, v_eventdata);
+
+			-- if filtering is allowed
+			IF v_conf ? 'filter' THEN
+				-- see which workers have matching filters
+				SELECT
+					array_agg(worker_id) INTO v_workers
+				FROM
+					worker_actions
+				WHERE
+					action_id = v_action_id
+					AND (filter IS NULL
+					     OR v_in_args @> filter);
+
+				IF v_workers IS NULL THEN
+					RAISE NOTICE 'no worker for action_id % in_args %', v_action_id, v_in_args;
+					CONTINUE;
+				END IF;
+				-- RAISE NOTICE 'action_id % in_args % workers %',	v_action_id, v_in_args, v_workers;
+				v_payload = jsonb_build_object('job_id', v_job_id, 'workers', v_workers);
+			ELSE
+				v_payload = jsonb_build_object('job_id', v_job_id);
+			END IF;
+
 			RAISE NOTICE 'retry action % for %', v_action_id, v_job_id;
-			PERFORM pg_notify('action:' || v_action_id || ':ready', v_job_id::text);
+			RAISE NOTICE 'NOTIFY "action:%:ready", %', v_action_id, v_payload;
+			PERFORM pg_notify('action:' || v_action_id || ':ready', v_payload::text);
 		WHEN 'working' THEN
 			v_eventdata = jsonb_build_object(
 				'error', jsonb_build_object(
