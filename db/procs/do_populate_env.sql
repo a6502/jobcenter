fdc365431c5d71d64cdb7bf8207beef82185d3a8
Wieger Opmeer <opmeer@strato-rz.de>
2017-09-11 20:18:03 +0200

    rewrite do_populate_env as plain sql

--- db/procs/do_populate_env.sql
+++ db/procs/do_populate_env.sql
@@ -1,16 +1,13 @@
 CREATE OR REPLACE FUNCTION jobcenter.do_populate_env(a_jobtask jobtask, a_env jsonb)
  RETURNS jsonb
- LANGUAGE plpgsql
+ LANGUAGE sql
+ IMMUTABLE
  SET search_path TO jobcenter, pg_catalog, pg_temp
 AS $function$
-BEGIN
-	IF a_env IS NULL THEN
-		a_env = '{}'::jsonb;
-	END IF;
-
-	a_env := jsonb_set(a_env, '{workflow_id}', to_jsonb(a_jobtask.workflow_id));
-	a_env := jsonb_set(a_env, '{job_id}', to_jsonb(a_jobtask.job_id));
-	a_env := jsonb_set(a_env, '{task_id}', to_jsonb(a_jobtask.task_id));
-
-	RETURN a_env;
-END;$function$
+SELECT
+	COALESCE(a_env, '{}'::jsonb)
+	|| jsonb_build_object(
+		'workflow_id', a_jobtask.workflow_id,
+		'job_id', a_jobtask.job_id,
+		'task_id', a_jobtask.task_id);
+$function$
