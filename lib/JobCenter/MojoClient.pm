88f2859baacd7a8c0182fbb4650d43e1659bade1
Wieger Opmeer <opmeer@strato-rz.de>
2017-12-08 13:45:14 +0100

    repair the original interface

--- lib/JobCenter/MojoClient.pm
+++ lib/JobCenter/MojoClient.pm
@@ -1,9 +1,5 @@
 package JobCenter::MojoClient;
 
-use strict;
-use warnings;
-use 5.10.0;
-
 #
 # Mojo's default reactor uses EV, and EV does not play nice with signals
 # without some handholding. We either can try to detect EV and do the
@@ -14,9 +10,8 @@ BEGIN {
 }
 
 # mojo
-use Mojo::Base 'Mojo::EventEmitter';
+use Mojo::Base -base;
 use Mojo::IOLoop;
-#use Mojo::JSON qw(decode_json encode_json);
 use Mojo::Log;
 use Mojo::Pg;
 
@@ -72,11 +67,11 @@ sub new {
 	$self->{json} = $args{json} // 1;
 	$self->{log} = $args{log} // Mojo::Log->new;
 	$self->{timeout} = $args{timeout} // 60;
-	$self->catch(sub { my ($self, $err) = @_; say "This looks bad: $err"; });
+	#$self->catch(sub { my ($self, $err) = @_; say "This looks bad: $err"; });
 	return $self;
 }
 
-sub _poll_done {
+sub _poll_done_old {
 	my ($self, $job) = @_;
 	my $res = $self->pg->db->dollar_only->query(q[select * from get_job_status($1)], $job->job_id)->array;
 	return unless $res and @$res and @$res[0];
@@ -87,7 +82,7 @@ sub _poll_done {
 		$outargs = decode_json($outargs);
 	}
 	if ($job->cb) {
-		$self->log->debug("calling cb $job->{cb} for job_id $job->{job_id} outargs $outargs");
+
 		local $@;
 		eval { $job->cb->($job->{job_id}, $outargs); };
 		$self->log->debug("got $@ calling callback") if $@;
@@ -95,10 +90,43 @@ sub _poll_done {
 	return $outargs; # at least true
 }
 
+sub _poll_done {
+	my ($self, $job) = @_;
+	Mojo::IOLoop->delay->steps(
+		sub {
+			my $d = shift;
+			$self->pg->db->dollar_only->query(
+				q[select * from get_job_status($1)],
+				$job->job_id,
+				$d->begin
+			);
+		},
+		sub {
+			my ($d, $err, $res) = @_;
+			die $err if $err;
+			my ($outargs) = @{$res->array};
+			return unless $outargs;
+			#$self->pg->pubsub->unlisten($job->listenstring);
+			$self->pg->pubsub->unlisten('job:finished', $job->lcb);
+			Mojo::IOLoop->remove($job->tmr) if $job->tmr;
+			$self->log->debug("calling cb $job->{cb} for job_id $job->{job_id} outargs $outargs");
+			my $outargsp;
+			local $@;
+			eval { $outargsp = decode_json(encode_utf8($outargs)); };
+			$outargsp = { error => 'error decoding json: ' . $outargs } if $@;
+			eval { $job->cb->($job->{job_id}, $outargsp); };
+			$self->log->debug("got $@ calling callback") if $@;
+			$job->delete;
+		}
+	);
+}
+
 sub call {
 	my ($self, %args) = @_;
 	my ($done, $job_id, $outargs);
-	$args{cb} = sub {
+	$args{waitcb} = sub{ say 'foo';};
+	$args{resultcb} = sub {
+		say 'bar', Dumper(\@_);
 		($job_id, $outargs) = @_;
 		$done++;
 	};
@@ -109,7 +137,7 @@ sub call {
 	return $job_id, $outargs;
 }
 
-sub call_nb {
+sub call_nb_old {
 	my ($self, %args) = @_;
 	my $wfname = $args{wfname} or die 'no workflowname?';
 	my $vtag = $args{vtag};
@@ -179,6 +207,111 @@ sub call_nb {
 
 	return $job_id;
 }
+
+sub call_nb {
+	my ($self, %args) = @_;
+	my $wfname = $args{wfname} or die 'no workflowname?';
+	my $vtag = $args{vtag};
+	my $inargs = $args{inargs} // '{}';
+	my $waitcb = $args{waitcb} or die 'no wait callback?';
+	my $resultcb = $args{resultcb} or die 'no result callback?';
+	my $timeout = $args{timeout} // 60;
+
+	if ($self->{json}) {
+		# sanity check json string
+		my $inargsp = decode_json($inargs);
+		die 'inargs is not a json object' unless ref $inargsp eq 'HASH';
+	} else {
+		die  'inargs should be a hashref' unless ref $inargs eq 'HASH';
+		$inargs = encode_json($inargs);
+		#$self->log->debug("inargs as json: $inargs");
+	}
+
+	my $impersonate; # = $client->who;
+	my $env;
+
+	#$inargs = decode_utf8(encode_json($inargs));
+
+	$self->log->debug("calling $wfname with '$inargs'" . (($vtag) ? " (vtag $vtag)" : ''));
+
+	# create_job throws an error when:
+	# - wfname does not exist
+	# - inargs not valid
+	Mojo::IOLoop->delay->steps(
+		sub {
+			my $d = shift;
+			#($job_id, $listenstring) = @{
+			$self->pg->db->dollar_only->query(
+				q[select * from create_job(wfname := $1, args := $2, tag := $3, impersonate := $4, env := $5)],
+				$wfname,
+				$inargs,
+				$vtag,
+				$impersonate,
+				$env,
+				$d->begin
+			);
+		},
+		sub {
+			my ($d, $err, $res) = @_;
+
+			if ($err) {
+				$resultcb->(undef, $err);
+				return;
+			}
+			my ($job_id, $listenstring) = @{$res->array};
+			unless ($job_id) {
+				$resultcb->(undef, "no result from call to create_job");
+				return;
+			}
+
+			# report back to our caller immediately
+			# this prevents the job_done notification overtaking the 
+			# 'job created' result...
+			$self->log->debug("created job_id $job_id listenstring $listenstring");
+			$waitcb->($job_id, undef);
+
+			my $job = JobCenter::Api::Job->new(
+				cb => $resultcb,
+				job_id => $job_id,
+				inargs => $inargs,
+				listenstring => $listenstring,
+				vtag => $vtag,
+				wfname => $wfname,
+			);
+
+			#$self->pg->pubsub->listen($listenstring, sub {
+			# fixme: 1 central listen?
+			my $lcb = $self->pg->pubsub->listen('job:finished', sub {
+				my ($pubsub, $payload) = @_;
+				$self->log->debug("notify job:finished $payload");
+				return unless $job_id == $payload;
+				local $@;
+				eval { $self->_poll_done($job); };
+				$self->log->debug("pubsub cb $@") if $@;
+			});
+
+			my $tmr;
+			$tmr = Mojo::IOLoop->timer($timeout => sub {
+				# request failed, cleanup
+				#$self->pg->pubsub->unlisten($listenstring);
+				$self->pg->pubsub->unlisten('job:finished' => $lcb);
+				# the cb might fail if the connection is gone..
+				eval { &$resultcb->($job_id, {'error' => 'timeout'}); };
+				$job->delete;
+			}) if $timeout > 0;
+			#$self->log->debug("setting tmr: $tmr") if $tmr;
+
+			$job->update(tmr => $tmr, lcb => $lcb);
+
+			# do one poll first..
+			$self->_poll_done($job);
+		}
+	)->catch(sub {
+		my ($delay, $err) = @_;
+		$resultcb->(undef, $err);
+	});
+}
+
 
 
 1;
