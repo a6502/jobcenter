2fa490b815b998403ca3e58ed22cef8fdc16919d
Wieger Opmeer <opmeer@strato-rz.de>
2018-10-23 19:29:39 +0200

    disconnect is not longer a special case of withdraw

--- db/procs/disconnect.sql
+++ db/procs/disconnect.sql
@@ -2,13 +2,48 @@ CREATE OR REPLACE FUNCTION jobcenter.disconnect(a_workername text)
  RETURNS boolean
  LANGUAGE plpgsql
  SECURITY DEFINER
- SET search_path TO jobcenter, pg_catalog, pg_temp
-AS $function$
+ SET search_path TO 'jobcenter', 'pg_catalog', 'pg_temp'
+AS $function$DECLARE
+	v_worker_id bigint;
+	v_error jsonb;
+	v_job_id bigint;
+	v_task_id int;
+	v_workflow_id int;
 BEGIN
-	RETURN BOOL_AND(do_withdraw(a_workername, a.name, disconnecting => TRUE)) 
-		FROM workers AS w 
-		JOIN worker_actions AS wa USING(worker_id) 
-		JOIN actions AS a USING(action_id) 
-		WHERE w.name = a_workername;
-END$function$
+	UPDATE workers SET
+		stopped = now()
+	WHERE
+		name = a_workername
+		AND stopped IS NULL
+	RETURNING
+		worker_id INTO v_worker_id;
 
+	IF NOT FOUND THEN
+		RETURN FALSE;
+	END IF;
+
+	DELETE FROM
+		 worker_actions
+	WHERE worker_id = v_worker_id;
+
+	-- add workername to msg?
+	v_error := '{"error":{"class": "soft", "msg": "worker disconnected"}}'::jsonb;
+
+	FOR v_job_id, v_task_id, v_workflow_id IN
+		SELECT
+			 job_id, task_id, workflow_id
+		FROM
+			jobs
+			JOIN tasks USING (workflow_id, task_id)
+			JOIN actions USING (action_id)
+		WHERE
+			state = 'working'
+			AND (task_state->>'worker_id')::bigint = v_worker_id
+			AND (config->>'restartable')::boolean = true
+	LOOP
+		RAISE LOG 'raising error in %s', (v_workflow_id, v_task_id, v_job_id)::jobtask;
+		PERFORM do_task_error((v_workflow_id, v_task_id, v_job_id)::jobtask, v_error);
+	END LOOP;
+
+	RETURN TRUE;
+END$function$
