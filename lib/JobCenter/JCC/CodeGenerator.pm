b1d5f9feb6384c30bf55b09e583f025ebc087b3f
Wieger Opmeer <opmeer@strato-rz.de>
2018-10-23 19:41:27 +0200

    add 'retryable' config flag, remove 'persistent'

--- lib/JobCenter/JCC/CodeGenerator.pm
+++ lib/JobCenter/JCC/CodeGenerator.pm
@@ -50,7 +50,7 @@ our %cfgcfg = (
 		filter => 'array',
 		retry => 'object',
 		timeout => 'duration',
-		persistent  => 'boolean',
+		retryable  => 'boolean',
 	},
 	workflow => {
 		max_depth => 'number',
