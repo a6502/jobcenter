6e2a3efbb0c5d1f7a824aa8bb85cb0bc7c912c84
Michael Green <green@strato.de>
2018-11-12 18:41:39 +0100

    add workflow call statistics to archive and cleanup process

---
+++ db/schema/v15/alter.sql
@@ -0,0 +1,24 @@
+CREATE TABLE jobcenter.call_stats (
+    name text NOT NULL PRIMARY KEY,
+    calls bigint DEFAULT 0 NOT NULL
+);
+
+
+ALTER TABLE jobcenter.call_stats OWNER TO $JCSYSTEM;
+
+
+CREATE TABLE jobcenter.call_stats_collected (
+	unique_id boolean NOT NULL PRIMARY KEY DEFAULT TRUE,
+    last timestamp with time zone DEFAULT now(),
+	CONSTRAINT call_stats_collected_urow CHECK (unique_id)
+);
+
+
+ALTER TABLE jobcenter.call_stats_collected OWNER TO $JCSYSTEM;
+
+
+DO $BODY$
+BEGIN
+        RAISE NOTICE 'Please load super.sql as a Postgresql superuser.';
+END
+$BODY$;
