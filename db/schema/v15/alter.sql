30eaea9085bdd0acdc0ff911d5beb6fceec92806
Michael Green <green@strato.de>
2018-11-13 12:16:37 +0100

    fix tabs

--- db/schema/v15/alter.sql
+++ db/schema/v15/alter.sql
@@ -1,6 +1,6 @@
 CREATE TABLE jobcenter.call_stats (
-    name text NOT NULL PRIMARY KEY,
-    calls bigint DEFAULT 0 NOT NULL
+	name text NOT NULL PRIMARY KEY,
+	calls bigint DEFAULT 0 NOT NULL
 );
 
 
@@ -9,7 +9,7 @@ ALTER TABLE jobcenter.call_stats OWNER TO $JCSYSTEM;
 
 CREATE TABLE jobcenter.call_stats_collected (
 	unique_id boolean NOT NULL PRIMARY KEY DEFAULT TRUE,
-    last timestamp with time zone DEFAULT now(),
+	last timestamp with time zone DEFAULT now(),
 	CONSTRAINT call_stats_collected_urow CHECK (unique_id)
 );
 
