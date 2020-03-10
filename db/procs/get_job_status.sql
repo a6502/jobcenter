884b45f3fab2512cb85b0e314a134804c76980bd
Wieger Opmeer <wieger@a6502.net>
2020-03-10 12:53:47 +0100

    don't use a union query because we know what the optimizer doesn't know: a job_id can only be in one table, and most likely that's the jobs table

--- db/procs/get_job_status.sql
+++ db/procs/get_job_status.sql
@@ -12,19 +12,22 @@ BEGIN
 	FROM
 		jobs
 	WHERE
-		job_id = a_job_id
-	UNION ALL SELECT
-		state, out_args
-	FROM
-		jobs_archive
-	WHERE
 		job_id = a_job_id;
 
-	IF NOT FOUND THEN
-		--RAISE EXCEPTION 'no job %', a_job_id;
-		o_out_args := format('{"error":"no job %s"}', a_job_id)::jsonb;
-		a_job_id := null;
-		RETURN;
+	IF NOT FOUND THEN -- in the archive then?
+		SELECT
+			state, out_args INTO v_state, o_out_args
+		FROM
+			jobs_archive
+		WHERE
+			job_id = a_job_id;
+
+		IF NOT FOUND THEN
+			--RAISE EXCEPTION 'no job %', a_job_id;
+			o_out_args := format('{"error":"no job %s"}', a_job_id)::jsonb;
+			a_job_id := null;
+			RETURN;
+		END IF;
 	END IF;	
 
 	IF v_state NOT IN ('error', 'finished') THEN
